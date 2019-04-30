28d1a2178d260cb42946896de3f71da1a4213968
gricoj <georgerico417@gmail.com>
2019-04-30 14:00:52 -0500

    Create DiscordNotification.ps1

---
+++ DiscordNotification.ps1
@@ -0,0 +1,34 @@
+$ComputerName = $env:computername
+$DateAndTime = Get-Date -Format g
+$Model = (Get-WmiObject -Class Win32_ComputerSystem).Model
+$Manufacturer = (Get-WmiObject -Class Win32_BIOS).Manufacturer
+$SN = (Get-WmiObject win32_bios).SerialNumber
+$MacAddress = Get-NetAdapter -Name Ethernet | Select MacAddress
+
+#Getting OU of the local computer
+$ADSISearcher = New-Object System.DirectoryServices.DirectorySearcher;
+$ADSISearcher.Filter = '(&(name=' + $ComputerName + ')(objectClass=computer))';
+$ADSISearcher.SearchScope = 'Subtree';
+$Computer = $ADSISearcher.FindAll();
+
+$OU = $($Computer.Properties.Item('distinguishedName')).Substring($($Computer.Properties.Item('distinguishedName')).IndexOf('OU='))
+
+
+
+#Sending the message
+$url = "Enter Webhook URL in these quotes"
+$content = @"
+#<@&......> Role ID can be retrieved by calling \@ROLENAME
+__**Task Sequence completed**__
+**Time Completed:** $DateAndTime
+**Computer Name:** $ComputerName
+**MAC Address:** $MacAddress
+**Manufacturer:** $Manufacturer
+**Computer Model:** $Model
+**Serial Number:** $SN
+**OU:** $OU
+"@
+$payload = [PSCustomObject]@{
+    content = $content
+    }
+Invoke-RestMethod -Uri $url -Method Post -Body ($payload | ConvertTo-Json)
