545589fef18e7fcc64f430de169704a740ac8529
George Rico Juarez <georgerico417@gmail.com>
2019-04-30 14:22:55 -0500

    Add files via upload

---
+++ DiscordNotification.ps1
@@ -0,0 +1,34 @@
+﻿$ComputerName = $env:computername
+$DateAndTime = Get-Date -Format g
+$Model = (Get-WmiObject -Class Win32_ComputerSystem).Model
+$Manufacturer = (Get-WmiObject -Class Win32_BIOS).Manufacturer
+$SN = (Get-WmiObject win32_bios).SerialNumber
+$MacAddress = Get-NetAdapter -Name Ethernet | Select MacAddress
+
+#Getting OU of the local computer
+$ADSISearcher = New-Object System.DirectoryServices.DirectorySearcher;
+$ADSISearcher.Filter = '(&(name=' + $ComputerName + ')(objectClass=computer))';
+$ADSISearcher.SearchScope = 'Subtree';
+$Computer = $ADSISearcher.FindAll();
+
+$OU = $($Computer.Properties.Item('distinguishedName')).Substring($($Computer.Properties.Item('distinguishedName')).IndexOf('OU='))
+
+
+
+#Sending the message
+$url = "https://discordapp.com/api/webhooks/463355360749682748/olFqzc1R8_Uo4X0duYPPpnI1Ygl7Rl3soxqw48CnyJfe_eaHedoBB7SqkvtWu7Z603wg"
+$content = @"
+<@&463371572560920606>
+__**Task Sequence completed**__
+**Time Completed:** $DateAndTime
+**Computer Name:** $ComputerName
+**MAC Address:** $MacAddress
+**Manufacturer:** $Manufacturer
+**Computer Model:** $Model
+**Serial Number:** $SN
+**OU:** $OU
+"@
+$payload = [PSCustomObject]@{
+    content = $content
+    }
+Invoke-RestMethod -Uri $url -Method Post -Body ($payload | ConvertTo-Json)
